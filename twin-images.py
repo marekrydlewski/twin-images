6f41fdd6ed3592c7191fb6a74f72dc9c3fa59d76
Marek Rydlewski <marek.rydlewski@outlook.com>
2017-05-12 16:19:31 +0200

    Setup project

---
+++ twin-images.py
@@ -0,0 +1,13 @@
+# http://www.cs.put.poznan.pl/bwieloch/?page_id=653
+
+
+def extract(image, keypoints):
+    pass
+
+
+def distance(descriptor1, descriptor2):
+    pass
+
+
+if __name__ == "__main__":
+    print("Twin Image descriptor")
